5720e27abfed2daf7f5916b62091624e22d5d6a3
Nevnet99 <luke@dexla.ai>
2023-09-28 16:24:18 +0100

    fix: try static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/static';
 import { defineConfig } from 'astro/config';
-
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
